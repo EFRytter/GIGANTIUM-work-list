1cc3fb3c5142ae4e6cd97a892cbf412bae7b0c08
Kenan <RytterElisa@gmail.com>
2026-07-29 21:33:46 +0200

    date

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template # Imports the Flask class from the flask package
+from flask import Flask, render_template, request
+from datetime import date
+import calendar
 
 app = Flask(__name__)  # Creates the Flask application instance
 
-@app.route('/')  # Connects the homepage address to the function below
-def home():  # Runs whenever someone visits the homepage
-    return render_template("index.html")  # Access the index.html template and shows the content
+days_in_a_week = [
+    "Mandag",
+    "Tirsdag",
+    "Onsdag",
+    "Torsdag",
+    "Fredag",
+    "Lørdag",
+    "Søndag",
+]
 
-app.run(debug=True)  # Starts the server; auto-restarts on changes, shows detailed errors
+@app.route('/')  # Connects the homepage address to the function below
+def home(): 
+
+    my_date = date.today()      # Gets todays date
+    today = calendar.day_name[my_date.weekday()] # converts that date into a weekday name
+
+    day = request.args.get("day", today)
+
+    return render_template("index.html", day=day, today=today, day_list=days_in_a_week)  # Access the index.html template
+
+app.run(debug=True)
+
+
+
