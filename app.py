b143b7961f1e41bc32d0e9ad3234130eb556c2c7
Kenan <RytterElisa@gmail.com>
2026-08-07 21:38:03 +0200

    mn

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import date, timedelta
 import calendar
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)  # Creates the Flask application instance
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class tasks(db.Model):
+    _id = db.Column("id",db.Integer, primary_key=True)
+    title = db.Column (db.String(100))
+    description = db.Column (db.String(500))
+    time_of_day = db.Column(db.String(100))
+    weekday = db.Column(db.String(100))
+    image = db.Column(db.String(100))
+    completed = db.Column(db.Boolean, default=False)
+
+
+    def __init__(self, title, description,time_of_day,weekday,image,completed):
+        self.title = title
+        self.description = description
+        self.time_of_day = time_of_day
+        self.weekday = weekday
+        self.image = image
+        self.completed = completed
+
+
 
 days_in_a_week = [
     {"english": "Monday", "danish": "Mandag"},
@@ -13,7 +38,6 @@ days_in_a_week = [
     {"english": "Saturday", "danish": "Lørdag"},
     {"english": "Sunday", "danish": "Søndag"},
 ]
-tasks = []
 
 @app.route('/')  # Connects the homepage address to the function below
 def home(): 
@@ -32,7 +56,7 @@ def home():
     day = request.args.get("day", today) #read a URL parameter called day, if missing, its using today as a fallback
     
     
-    return render_template("index.html", day=day, today=today, day_list=days_in_a_week, week=week, week_number=current_week_number, tasks=tasks)  # Access the index.html template
+    return render_template("index.html", day=day, today=today, day_list=days_in_a_week, week=week, week_number=current_week_number, tasks=tasks.query.all())  # Access the index.html template
 
 @app.route('/add-task', methods=["GET","POST"])
 def add_task():
@@ -47,15 +71,18 @@ def add_task():
             image.save("static/uploads/" + image.filename)
         else:
             image = None
-
-        new_task = {"title": title, "description": description, "time_of_day": time_of_day, "image": image.filename if image else None, "weekday": weekday}
-        tasks.append(new_task)
+        new_task = tasks(title=title, description=description, time_of_day=time_of_day, weekday=weekday, image=image.filename if image else None, completed=False)
+        db.session.add(new_task)
+        db.session.commit()
         
         return redirect(url_for("home"))
     
     return render_template("add-task.html")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
 
 
 
